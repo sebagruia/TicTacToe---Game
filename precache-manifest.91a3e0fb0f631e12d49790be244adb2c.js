f0675d03d4af3c2f149bf18a561fb6aa931af0ae
sebagruia <sebastiangruia@gmail.com>
2019-11-21 22:06:45 +0100

    Updates

---
+++ precache-manifest.91a3e0fb0f631e12d49790be244adb2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26b6b040c6e10f62a49d975799a9126",
+    "url": "/TicTacToe---Game/index.html"
+  },
+  {
+    "revision": "bdfe005eb9af3d1587d4",
+    "url": "/TicTacToe---Game/static/css/main.7f367169.chunk.css"
+  },
+  {
+    "revision": "5c5bcffba3e3ba07d31c",
+    "url": "/TicTacToe---Game/static/js/2.c3356a4a.chunk.js"
+  },
+  {
+    "revision": "bdfe005eb9af3d1587d4",
+    "url": "/TicTacToe---Game/static/js/main.246af716.chunk.js"
+  },
+  {
+    "revision": "517acf21e328fc691610",
+    "url": "/TicTacToe---Game/static/js/runtime-main.dca8a78b.js"
+  }
+]);
